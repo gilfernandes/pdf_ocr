dcfabf617982e09498310a49a09c92be82b4e613
Gil Fernandes <gil.fernandes@onepointltd.com>
2023-08-06 15:49:04 +0100

    corrected for wheel generation

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name="pdf_image_ocr", version="1.0", packages=find_packages())
+setup(name="pdf_image_ocr", version="1.0", packages=['pdf_image_ocr'])
